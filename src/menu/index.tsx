9d9b669a18e7802989a548cb621c52f65cba05b9
Bruno Lima <bru.no@outlook.com.br>
2024-01-19 22:07:03 -0300

    Criação do componente "menu"

---
+++ src/menu/index.tsx
@@ -0,0 +1,18 @@
+import './style.css'
+export default function Menu() {
+    return (
+        <div className='menu'>
+            <ul className='menu_lista'>
+                
+                <li><a href='inicio' className='link_menu'>Início</a></li>
+
+                <li><a href='noticias' className='link_menu'>Notícias</a></li>
+                
+                <li><a href='edicoes' className='link_menu'>Edições</a></li>
+                
+                <li><a href='sobre' className='link_menu'>Sobre</a></li>
+            </ul>
+        </div>
+    )
+
+}
