bea2ef0be423fa3fdf7322b7f4d70d655b9862cd
Bruno Lima <bru.no@outlook.com.br>
2024-01-25 16:39:05 -0300

    Composição do componente Banner na página inicial do site

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/src/header"
+import Banner from "@/src/banner"
 import Footer from "@/src/footer"
 import Menu from "@/src/menu"
+
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <Banner/>
       <Footer/>
       <Menu/>
     </main>
