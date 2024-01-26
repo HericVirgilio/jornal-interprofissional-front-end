4731409d3dbb5db12dcf6080be7a361da4baedf9
Bruno Lima <bru.no@outlook.com.br>
2024-01-26 15:25:17 -0300

    Acrescentado efeito de transição no Banner

--- src/banner/index.tsx
+++ src/banner/index.tsx
@@ -1,25 +1,33 @@
 "use client"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade } from 'swiper/modules'
+import Image from 'next/image';
+import './style.css'
 
 export default function Banner() {
-    const data = [
-        { id: '1', image: 'https://jornalinterprofissional.online/wp-content/uploads/2023/12/jornalinterprofissional-e1701790458297.jpeg', alt: 'Banner nº 1 - Jornal Interprofissional' },
+    const images = [
+        { id: '1', image: '/images/banners/Banner1.png', alt: 'Banner nº 1 - Jornal Interprofissional' },
+        { id: '2', image: '/images/banners/Banner2.png', alt: 'Banner Estamos em Manutenção! Voltaremos em breve' },
     ];
 
     return (
         <div>
             <Swiper
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                navigation
+                modules={[EffectFade]}
+                effect='fade'
+                speed={500}
+                loop={true}
             >
-                {data.map(item => (
+                {images.map(item => (
                     <SwiperSlide key={item.id}>
-                        <img
+                        <Image
                             src={item.image}
                             alt={item.alt}
-                            className='slide-item'
+                            width={1920}
+                            height={500}
+                            className='banner'
+                            priority='false'
                         />
                     </SwiperSlide>
                 ))}
