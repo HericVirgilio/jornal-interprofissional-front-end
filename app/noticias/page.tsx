284e10e55d8acac38b89597874aaa74e20d5a55c
ClaudioPalles <44623225+ClaudioPalles@users.noreply.github.com>
2024-01-26 20:36:55 -0300

    Update page.tsx

--- app/noticias/page.tsx
+++ app/noticias/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/src/header"
 import Footer from "@/src/footer"
 import Menu from "@/src/menu"
+import Implementation from "@/src/Implementation"
 
 
 export default function Page() {
   return (
     <main>
       <Header/>
+      <Implementation/>
         <div>
           <h1>Notícias</h1>
         </div>
