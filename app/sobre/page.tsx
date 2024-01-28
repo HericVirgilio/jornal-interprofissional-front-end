60c21b8f7ce885843fb9c64e550c5d3fa99f8ecb
Bruno Lima <bru.no@outlook.com.br>
2024-01-28 19:45:28 -0300

    Composição da página "Sobre nós"

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -1,17 +1,15 @@
-import Header from "@/src/header"
-import Footer from "@/src/footer"
-import Menu from "@/src/menu"
-
+import Header from "@/src/header";
+import Footer from "@/src/footer";
+import Sobre from "@/src/sobre";
+import Menu from "@/src/menu";
 
 export default function Page() {
   return (
     <main>
-      <Header/>
-        <div>
-          <h1>Sobre</h1>
-        </div>
-      <Footer/>
-      <Menu/>
+      <Header />
+      <Sobre />
+      <Footer />
+      <Menu />
     </main>
-  )
+  );
 }
