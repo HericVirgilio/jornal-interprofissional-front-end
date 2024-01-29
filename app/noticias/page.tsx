d89969bc4fd157fc82fc24c42a17200597277e56
Bruno Lima <bru.no@outlook.com.br>
2024-01-29 19:28:49 -0300

    Refatoração do componente

--- app/noticias/page.tsx
+++ app/noticias/page.tsx
@@ -1,17 +1,14 @@
 import Header from "@/src/header"
 import Footer from "@/src/footer"
 import Menu from "@/src/menu"
-import Implementation from "@/src/Implementation"
+import News from "@/src/News"
 
 
 export default function Page() {
   return (
     <main>
       <Header/>
-      <Implementation/>
-        <div>
-          <h1>Notícias</h1>
-        </div>
+      <News/>
       <Footer/>
       <Menu/>
     </main>
---
+++ src/news/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import './style.css'
+
+export default function News() {
+    return (
+
+        <div>
+
+            <h1 className="titulo-pagina">Notícias</h1>
+
+            <div className="noticias">
+
+                <div className="materia">
+                    <Image className='imgmateria' src="/images/news/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
+                    <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                </div>
+
+                <div className="materia">
+                    <Image className='imgmateria' src="/images/news/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
+                    <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                </div>
+
+                <div className="materia">
+                    <Image className='imgmateria' src="/images/news/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
+                    <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
