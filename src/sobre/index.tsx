0fc8d8e5dd540b5a1766d177b8ce8d538939993f
Bruno Lima <bru.no@outlook.com.br>
2024-01-29 00:26:28 -0300

    Refatoração do componente Sobre

--- src/sobre/index.tsx
+++ src/sobre/index.tsx
@@ -1,74 +1,82 @@
 import Image from "next/image"
+import './style.css'
 
 export default function Sobre() {
     return (
         <div>
-            <h1>Sobre nós</h1>
+            <h1 className="titulo-pagina">Sobre nós</h1>
 
-            <div>
-                <Image
-                    src='/images/devs/Bruno.jpg'
-                    alt='Foto do Bruno'
-                    width={320}
-                    height={320}
-                    className='img-dev'
-                    priority='false'
-                />
+            <div className="sobre">
 
-                <h2>Bruno Moreira Lima</h2>
+                <div className="card-sobre">
 
-                <p>Cosplayer, apaixonado por Impressão 3D e por contruir “coisas” com Arduíno. Bruno é o que está sempre envolvido nos projetos que acontecem, sempre levando consigo o Just Dance e o Beat Saber para animar qualquer evento.</p>
+                    <h2>Bruno Moreira Lima</h2>
 
-                <span>Instagram</span>
+                    <Image
+                        src='/images/devs/Bruno.jpg'
+                        alt='Foto do Bruno'
+                        width={320}
+                        height={320}
+                        className='img-dev'
+                        priority='false'
+                    />
 
-                <a href="https://www.instagram.com/bru.no_lima/" target="_blank">@bru.no_lima</a>
-                <a href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a>
-                <a href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a>
+                    <p className="card-descricao">Cosplayer, apaixonado por Impressão 3D e por contruir “coisas” com Arduíno. Bruno é o que está sempre envolvido nos projetos que acontecem, sempre levando consigo o Just Dance e o Beat Saber para animar qualquer evento.</p>
+
+                    <ul>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/bru.no_lima/" target="_blank">@bru.no_lima</a></li>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                    </ul>
+
+                </div>
+
+
+                <div className="card-sobre">
+
+                    <h2>Claudio Pales Costa</h2>
+
+                    <Image
+                        src='/images/devs/Claudio.jpg'
+                        alt='Foto do Claudio'
+                        width={320}
+                        height={320}
+                        className='img-dev'
+                        priority='false'
+                    />
+
+                    <p className="card-descricao">Primeira pessoa que vem a mente quando se fala de um bom Sumô Robô. Claudio passa a maior parte do seu tempo calibrando a mesa da Impressora 3D e está sempre topando qualquer projeto.</p>
+
+                    <ul>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a></li>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                    </ul>
+                </div>
+
+
+                <div className="card-sobre">
+
+                    <h2>Heric Virgilio Viana</h2>
+
+                    <Image
+                        src='/images/devs/Heric.jpg'
+                        alt='Foto do Heric'
+                        width={320}
+                        height={320}
+                        className='img-dev'
+                        priority='false'
+                    />
+
+                    <p className="card-descricao">Cabeça do Desenvolvimento Web da equipe, além de ser o membro mais experiente, está sempre mantendo a equipe unida e motivada para realizar a construção do site.</p>
+
+                    <ul>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a></li>
+                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                    </ul>
+                </div>
+
             </div>
-
-            
-            <div>
-                <Image
-                    src='/images/devs/Claudio.jpg'
-                    alt='Foto do Claudio'
-                    width={320}
-                    height={320}
-                    className='img-dev'
-                    priority='false'
-                />
-
-                <h2>Claudio Pales Costa</h2>
-
-                <p>Primeira pessoa que vem a mente quando se fala de um bom Sumô Robô. Claudio passa a maior parte do seu tempo calibrando a mesa da Impressora 3D e está sempre topando qualquer projeto.</p>
-
-                <span>Instagram</span>
-
-                <a href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a>
-                <a href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a>
-                <a href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a>
-            </div>
-
-            
-            <div>
-                <Image
-                    src='/images/devs/Heric.jpg'
-                    alt='Foto do Heric'
-                    width={320}
-                    height={320}
-                    className='img-dev'
-                    priority='false'
-                />
-
-                <h2>Heric Virgilio Viana</h2>
-
-                <p>Cabeça do Desenvolvimento Web da equipe, além de ser o membro mais experiente, está sempre mantendo a equipe unida e motivada para realizar a construção do site.</p>
-
-                <span>Instagram</span>
-
-                <a href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a>
-                <a href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a>
-            </div>
-        
         </div>
     )
 }
