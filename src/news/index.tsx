99b6e22e618ff074d387c14641af2dc3c6fbd314
Bruno Lima <bru.no@outlook.com.br>
2024-02-03 18:51:31 -0300

    Ajuste na estilização das notícias

--- src/news/index.tsx
+++ src/news/index.tsx
@@ -8,27 +8,27 @@ export default function News() {
 
             <h1 className="titulo-pagina">Notícias</h1>
 
-            <div className="noticias">
+            <div className="noticias-container">
 
                 <div className="materia">
                     <Image className='imgmateria' src="/images/covers/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
                     <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                    <a className='leiamais' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
                 </div>
 
                 <div className="materia">
                     <Image className='imgmateria' src="/images/covers/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
                     <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                    <a className='leiamais' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
                 </div>
 
                 <div className="materia">
                     <Image className='imgmateria' src="/images/covers/teste-capa.jpg" alt="Imagem de capa da notícia" width={360} height={360} />
                     <a className='titulomateria' href="https://www.instagram.com/plataformanerd.store/">Primeira edição do Jornal Interprofissional</a>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                    <a className='readmore' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
+                    <a className='leiamais' href="https://www.instagram.com/plataformanerd.store/">Leia mais »</a>
                 </div>
 
             </div>
