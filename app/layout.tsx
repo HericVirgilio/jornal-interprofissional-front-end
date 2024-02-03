1a1f25e39f7b445fbcb44f2ddc53f9150c559162
Bruno Lima <bru.no@outlook.com.br>
2024-02-03 13:50:55 -0300

    Remoção da biblioteca Swiper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import 'swiper/css'
-import 'swiper/css/effect-fade'
 
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
