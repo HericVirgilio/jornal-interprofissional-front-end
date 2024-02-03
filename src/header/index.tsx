2ebb567976676cd2a7e03d805d8033a45536a8f9
Bruno Lima <bru.no@outlook.com.br>
2024-02-03 18:48:39 -0300

    Adcionado efeito de diminiur altura do cabeçalho ao rolar a página para baixo

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -1,22 +1,45 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Menu from '../menu'
-import './style.css'
+"use client"
+
+import { useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from '../menu';
+import './style.css';
 
 export default function Header() {
-    return (
-        <div className='cabecalho'>
+  useEffect(() => {
+    const cabecalho = document.getElementById('cabecalho');
 
-            <Link href='/' className='titulo-site'><Image
-                src='/images/icons/logo.png'
-                width={50}
-                height={50}
-                alt='Logo Jornal Interprofissional'
-                className='logo-site'
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        cabecalho.classList.add('cabecalho-recolhido');
+        cabecalho.classList.remove('cabecalho-expandido');
+      } else {
+        cabecalho.classList.remove('cabecalho-recolhido');
+        cabecalho.classList.add('cabecalho-expandido');
+      }
+    }
 
-            />Jornal Interprofissional</Link>
+    window.addEventListener('scroll', handleScroll);
 
-            <Menu />
-        </div>
-    )
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  return (
+    <div className={`cabecalho-container cabecalho-expandido`} id='cabecalho'>
+      <Link href='/' className='titulo-site'>
+        <Image
+          src='/images/icons/logo.png'
+          width={50}
+          height={50}
+          alt='Logo Jornal Interprofissional'
+          className='logo-site'
+        />
+        Jornal Interprofissional
+      </Link>
+      <Menu />
+    </div>
+  )
 }
