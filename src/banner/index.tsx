424ed514f8728397dd0467c2a973dc2d68e97e79
Bruno Lima <bru.no@outlook.com.br>
2024-02-03 15:59:51 -0300

    Refatoração do componente Banner para ser compatível com vídeos

--- src/banner/index.tsx
+++ src/banner/index.tsx
@@ -1,70 +1,84 @@
 "use client"
 
 // Importa os módulos necessários do React e Next.js
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
 
 // Importa os estilos CSS
-import './style.css';
+import './style.css'
 
 // Componente funcional Banner
 const Banner = () => {
-  // Lista de objetos de imagens com id, caminho da imagem e texto alternativo
-  const images = [
-    { id: '1', image: '/images/banners/Banner1.png', alt: 'Banner nº 1 - Jornal Interprofissional' },
-    { id: '2', image: '/images/banners/Banner2.png', alt: 'Banner Estamos em Manutenção! Voltaremos em breve' },
+  // Lista de objetos de mídia com id, tipo (imagem ou vídeo), caminho e texto alternativo
+  const mediaList = [
+    { id: '1', type: 'image', source: '/images/banners/Banner1.png', alt: 'Banner nº 1 - Jornal Interprofissional' },
+    { id: '2', type: 'image', source: '/images/banners/Banner2.png', alt: 'Banner nº 2 - Jornal Interprofissional' },
+    { id: '3', type: 'video', source: '/images/banners/Banner3.webm', alt: 'Banner em vídeo nº 1 - Jornal Interprofissional' },
+    { id: '4', type: 'video', source: '/images/banners/Banner4.webm', alt: 'Banner Estamos em Manutenção! Voltaremos em breve' },
   ];
 
-  // Estado para controlar o índice da imagem atual
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // Estado para controlar o índice da mídia atual
+  const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
 
   // Função para lidar com o clique nos retângulos de navegação
   const handleRectangleClick = (index) => {
-    setCurrentImageIndex(index);
+    setCurrentMediaIndex(index);
   };
 
-  // Efeito de intervalo para alternar automaticamente as imagens a cada 10 segundos
+  // Efeito de intervalo para alternar automaticamente as mídias a cada 10 segundos
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setCurrentMediaIndex((prevIndex) => (prevIndex + 1) % mediaList.length);
     }, 10000);
 
     // Limpa o intervalo quando o componente é desmontado ou atualizado
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, [mediaList.length]);
 
   // Retorna o JSX do componente Banner
   return (
     <div className="banner-container">
-      {/* Mapeia as imagens e renderiza os slides no banner */}
-      {images.map((item, index) => (
+      {/* Mapeia as mídias e renderiza os slides no banner */}
+      {mediaList.map((item, index) => (
         <div
           key={item.id}
-          className={`banner-slide ${index === currentImageIndex ? 'visible' : ''}`}
+          className={`banner-slide ${index === currentMediaIndex ? 'visible' : ''}`}
           style={{
-            opacity: index === currentImageIndex ? 1 : 0,
+            opacity: index === currentMediaIndex ? 1 : 0,
             transition: 'opacity 1s ease-in-out',
           }}
         >
-          {/* Componente Image do Next.js para carregar as imagens responsivamente */}
-          <Image
-            src={item.image}
-            alt={item.alt}
-            layout="responsive"
-            width={1920}
-            height={500}
-            className='banner-image'
-            priority={false}
-          />
+          {/* Renderiza imagem ou vídeo com base no tipo de mídia */}
+          {item.type === 'image' ? (
+            <Image
+              src={item.source}
+              alt={item.alt}
+              width={1920}
+              height={500}
+              className='banner-image'
+              priority={false}
+            />
+          ) : (
+            <video
+              autoPlay
+              muted
+              loop
+              className='banner-video'
+            >
+              <source src={item.source} type="video/webm" />
+              Seu navegador não suporta essa funcionalidade.
+            </video>
+
+          )}
         </div>
       ))}
 
       {/* Navegação na parte inferior do banner - retângulos */}
       <div className="banner-navigation">
-        {images.map((_, index) => (
+        {mediaList.map((_, index) => (
           <div
             key={index}
-            className={`banner-rectangle ${index === currentImageIndex ? 'active' : ''}`}
+            className={`banner-rectangle ${index === currentMediaIndex ? 'active' : ''}`}
             onClick={() => handleRectangleClick(index)}
           />
         ))}
