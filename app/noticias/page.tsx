45f04ab0400734024f91d0fd5a09ffa37737d8b0
Bruno Lima <91264301+Blimabru@users.noreply.github.com>
2024-02-03 18:16:52 -0300

    Refatoração do componente

--- app/noticias/page.tsx
+++ app/noticias/page.tsx
@@ -1,7 +1,8 @@
 import Header from "@/src/header"
+import News from "@/src/news"
 import Footer from "@/src/footer"
 import Menu from "@/src/menu"
-import News from "@/src/News"
+
 
 
 export default function Page() {
