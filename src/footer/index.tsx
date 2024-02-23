bf15d84ea8c8d6f8c40cded7cf5e148f0afb1b00
Bruno Lima <bru.no@outlook.com.br>
2024-02-23 16:24:16 -0300

    Refatoração do código

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import './style.css'
+
 export default function Footer() {
     return (
         <div className='conjunto_rodape'>
@@ -7,11 +9,15 @@ export default function Footer() {
                 <h1 className='titulo_rodape'>Desenvolvido por:</h1>
 
                 <ul>
-                    <li><a href="https://www.instagram.com/bru.no_lima" className='link' target='_blank'><span className='cor_destaque_link'>@bru.no_lima</span></a>Bruno Lima</li>
-                    <li><a href="https://www.instagram.com/heric_viana_s/" className='link' target='_blank'><span className='cor_destaque_link'>@heric_viana_s</span></a>Heric Virgilio</li>
-                    <li><a href="https://www.instagram.com/claudio.pales" className='link' target='_blank'><span className='cor_destaque_link'>@claudio.pales</span></a>Claudio Pales</li>
-                    <li><a href="https://www.instagram.com/jpales_" className='link' target='_blank'><span className='cor_destaque_link'>@jpales_</span></a>João Pedro Pales</li>
+                    <li className="lista-rodape"><a href="https://www.instagram.com/bru.no_lima" className='link' target='_blank'><span className='cor_destaque_link'>@bru.no_lima</span></a>Bruno Lima</li>
+                    <li className="lista-rodape"><a href="https://www.instagram.com/heric_viana_s/" className='link' target='_blank'><span className='cor_destaque_link'>@heric_viana_s</span></a>Heric Virgilio</li>
+                    <li className="lista-rodape"><a href="https://www.instagram.com/claudio.pales" className='link' target='_blank'><span className='cor_destaque_link'>@claudio.pales</span></a>Claudio Pales</li>
+                    <li className="lista-rodape"><a href="https://www.instagram.com/jpales_" className='link' target='_blank'><span className='cor_destaque_link'>@jpales_</span></a>João Pedro Pales</li>
                 </ul>
+
+                <div className='link-privacidade'>
+                    <Link href='/privacidade'>Política de Privacidade e Termos de Uso</Link>
+                </div>
             </div>
         </div>
     )
--- src/sobre/index.tsx
+++ src/sobre/index.tsx
@@ -24,10 +24,10 @@ export default function Sobre() {
                     <p className="card-descricao">Cosplayer, apaixonado por Impressão 3D e por contruir “coisas” com Arduíno. Bruno é o que está sempre envolvido nos projetos que acontecem, sempre levando consigo o Just Dance e o Beat Saber para animar qualquer evento.</p>
 
                     <ul>
-                        <li><a className="cor_destaque_link_github link-github" href="https://github.com/Blimabru" target="_blank">Blimabru</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/bru.no_lima" target="_blank">@bru.no_lima</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                        <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/Blimabru" target="_blank">Blimabru</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/bru.no_lima" target="_blank">@bru.no_lima</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
 
                 </div>
@@ -49,10 +49,10 @@ export default function Sobre() {
                     <p className="card-descricao">Primeira pessoa que vem a mente quando se fala de um bom Sumô Robô. Claudio passa a maior parte do seu tempo calibrando a mesa da Impressora 3D e está sempre topando qualquer projeto.</p>
 
                     <ul>
-                    <li><a className="cor_destaque_link_github link-github" href="https://github.com/ClaudioPalles" target="_blank">ClaudioPalles</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                    <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/ClaudioPalles" target="_blank">ClaudioPalles</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
                 </div>
 
@@ -73,9 +73,9 @@ export default function Sobre() {
                     <p className="card-descricao">Membro mais experiente da equipe quando se trata de Desenvolvimento Web. Está sempre mantendo o grupo unido e motivado para realizar a construção do site. Isso quando não está assistindo animes, é claro.</p>
 
                     <ul>
-                        <li><a className="cor_destaque_link_github link-github" href="https://github.com/HericVirgilio" target="_blank">HericVirgilio</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a></li>
-                        <li><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                        <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/HericVirgilio" target="_blank">HericVirgilio</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a></li>
+                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
                 </div>
 
