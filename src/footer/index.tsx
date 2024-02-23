8b1f0bc8381f92f9e6a193bb4b75ffe5004fa724
Bruno Lima <bru.no@outlook.com.br>
2024-02-23 17:31:01 -0300

    Composição do componente Link_privacidade

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link_privacidade from '../link_privacidade'
 import './style.css'
 
 export default function Footer() {
@@ -7,16 +7,14 @@ export default function Footer() {
 
             <div className='creditos_rodape'>
                 <h1 className='titulo_rodape'>Desenvolvido por:</h1>
-
-                <ul>
-                    <li className="lista-rodape"><a href="https://www.instagram.com/bru.no_lima" className='link' target='_blank'><span className='cor_destaque_link'>@bru.no_lima</span></a>Bruno Lima</li>
-                    <li className="lista-rodape"><a href="https://www.instagram.com/heric_viana_s/" className='link' target='_blank'><span className='cor_destaque_link'>@heric_viana_s</span></a>Heric Virgilio</li>
-                    <li className="lista-rodape"><a href="https://www.instagram.com/claudio.pales" className='link' target='_blank'><span className='cor_destaque_link'>@claudio.pales</span></a>Claudio Pales</li>
-                    <li className="lista-rodape"><a href="https://www.instagram.com/jpales_" className='link' target='_blank'><span className='cor_destaque_link'>@jpales_</span></a>João Pedro Pales</li>
-                </ul>
-
-                <div className='link-privacidade'>
-                    <Link href='/privacidade'>Política de Privacidade e Termos de Uso</Link>
+                <div className='coluna-rodape'>
+                    <ul>
+                        <li className="lista-rodape"><a href="https://www.instagram.com/bru.no_lima" className='link' target='_blank'><span className='cor_destaque_link'>@bru.no_lima</span></a>Bruno Lima</li>
+                        <li className="lista-rodape"><a href="https://www.instagram.com/heric_viana_s/" className='link' target='_blank'><span className='cor_destaque_link'>@heric_viana_s</span></a>Heric Virgilio</li>
+                        <li className="lista-rodape"><a href="https://www.instagram.com/claudio.pales" className='link' target='_blank'><span className='cor_destaque_link'>@claudio.pales</span></a>Claudio Pales</li>
+                        <li className="lista-rodape"><a href="https://www.instagram.com/jpales_" className='link' target='_blank'><span className='cor_destaque_link'>@jpales_</span></a>João Pedro Pales</li>
+                    </ul>
+                    <Link_privacidade />
                 </div>
             </div>
         </div>
