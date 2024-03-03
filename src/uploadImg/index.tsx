cbc710d0fda064aed76adbe3e8fdc2ac8e0c046f
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-03-03 01:06:05 -0300

    corrigindo alguns erros

--- src/uploadImg/index.tsx
+++ src/uploadImg/index.tsx
@@ -1,30 +1,46 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
-import "./style.css"
-import Image from "next/image";
+import "./style.css";
+import axios from 'axios';
 
-export default function FileInputnNoticias() {
+export default function FileInputNoticias() {
 
     const [titulo, setTitulo] = useState<string>("");
     const [subTitulo, setSubTitulo] = useState<string>("");
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [selectedTexto, setSelectedTexto] = useState<string>("");
-    const [Form, setForm] = useState<FormData | null>(null);
 
     const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files && event.target.files[0];
         setSelectedFile(file);
     };
 
-    const EnviarFormParaAPI = (event: FormEvent) => {
+    const EnviarFormParaAPI = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData();
 
-        formData.append("selectedFile", selectedFile as File);
+        formData.append("file", selectedFile as File);
         formData.append("titulo", titulo);
         formData.append("preTexto", subTitulo);
         formData.append("textoNoticia", selectedTexto);
-        console.log(formData)
-        setForm(formData)
+
+
+        const requestData = {
+            titulo: titulo,
+            subtitulo: subTitulo,
+            texto: selectedTexto,
+            file: selectedFile
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8080/noticias/cria-noticia', requestData, {
+        headers: {
+            'Content-Type': 'multipart/form-data', // Certifique-se de definir o Content-Type como multipart/form-data
+        }
+    });
+            console.log('Response:', response.data);
+        } catch (error:any) {
+            console.error('Erro ao cadastrar usuário:', error.response);
+        }
     }
 
     return (
@@ -33,21 +49,19 @@ export default function FileInputnNoticias() {
 
             <form className="FormNoticias" onSubmit={EnviarFormParaAPI} >
 
-                <input className="InputText" type="text" placeholder="Titulo" onChange={(e) => setTitulo(e.target.value)} required />
+                <input className="InputText" type="text" placeholder="Titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)}  />
 
-                <textarea className="InputSubtitulo" placeholder="Pré-texto" onChange={(e) => setSubTitulo(e.target.value)} required />
+                <textarea className="InputSubtitulo" placeholder="Pré-texto" value={subTitulo} onChange={(e) => setSubTitulo(e.target.value)}  />
 
 
                 <label className="PictureLabel" style={{ backgroundImage: selectedFile ? `url(${URL.createObjectURL(selectedFile)})` : 'none', backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
-                    <input type="file" className="pictureInput" onChange={handleFileInputChange} required />
+                    <input type="file" className="pictureInput" onChange={handleFileInputChange}  />
                     <span className="SpanText">Enviar Imagem</span>
                 </label>
 
-                <textarea id="texto-noticia" rows={20} cols={100} placeholder="Digite seu texto" onChange={(e) => setSelectedTexto(e.target.value)} required />
-                <input type="submit" className="BotaoSubmit"/>
+                <textarea id="texto-noticia" rows={20} cols={100} placeholder="Digite seu texto" value={selectedTexto} onChange={(e) => setSelectedTexto(e.target.value)}  />
+                <input type="submit" className="BotaoSubmit" value="Enviar" />
             </form>
         </div>
     );
 }
-
-
