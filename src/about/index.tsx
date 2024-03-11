6ca406b29390a860540c4b41fc4b7702da1424cb
Blimabru <bru.no@outlook.com.br>
2024-03-10 21:50:46 -0300

    Ajuste no nome das classes

--- src/about/index.tsx
+++ src/about/index.tsx
@@ -1,4 +1,4 @@
-import Link_privacy from "@/src/link_privacy";
+import LinkPrivacy from "@/src/linkPrivacy";
 
 import Image from "next/image"
 import './style.css'
@@ -26,10 +26,10 @@ export default function About() {
                     <p className="card-descricao">Cosplayer, apaixonado por Impressão 3D e por contruir “coisas” com Arduíno. Bruno é o que está sempre envolvido nos projetos que acontecem, sempre levando consigo o Just Dance e o Beat Saber para animar qualquer evento.</p>
 
                     <ul>
-                        <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/Blimabru" target="_blank">Blimabru</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/bru.no_lima" target="_blank">@bru.no_lima</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link_github link-github" href="https://github.com/Blimabru" target="_blank">Blimabru</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/bru.no_lima" target="_blank">@bru.no_lima</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
 
                 </div>
@@ -51,10 +51,10 @@ export default function About() {
                     <p className="card-descricao">Primeira pessoa que vem a mente quando se fala de um bom Sumô Robô. Claudio passa a maior parte do seu tempo calibrando a mesa da Impressora 3D e está sempre topando qualquer projeto.</p>
 
                     <ul>
-                        <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/ClaudioPalles" target="_blank">ClaudioPalles</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link_github link-github" href="https://github.com/ClaudioPalles" target="_blank">ClaudioPalles</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/claudio.pales" target="_blank">@Claudio.Pales</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/PlataformaNerd.Store" target="_blank">@PlataformaNerd.Store</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
                 </div>
 
@@ -75,14 +75,14 @@ export default function About() {
                     <p className="card-descricao">Membro mais experiente da equipe quando se trata de Desenvolvimento Web. Está sempre mantendo o grupo unido e motivado para realizar a construção do site. Isso quando não está assistindo animes, é claro.</p>
 
                     <ul>
-                        <li className="lista"><a className="cor_destaque_link_github link-github" href="https://github.com/HericVirgilio" target="_blank">HericVirgilio</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a></li>
-                        <li className="lista"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link_github link-github" href="https://github.com/HericVirgilio" target="_blank">HericVirgilio</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/heric_viana_s" target="_blank">@heric_viana_s</a></li>
+                        <li className="lista-sobre"><a className="cor_destaque_link link-instagram" href="https://www.instagram.com/BitMakers.tech" target="_blank">@BitMakers.tech</a></li>
                     </ul>
                 </div>
             </div>
             <div className="link-privacidade-sobre">
-                <Link_privacy />
+                <LinkPrivacy />
             </div>
         </div>
     )
--- src/editions/index.tsx
+++ src/editions/index.tsx
@@ -1,25 +1,67 @@
-import Image from 'next/image'
-import './style.css'
+"use client"
+
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import axios from 'axios'; //npm install axios
+import './style.css';
+
+type Edicao = {
+  id: number;
+  titulo: string;
+  imagemAddress: string;
+  pdfAddress: string;
+  data: string;
+};
+
 export default function Editions() {
-    return (
+  const [edicoes, setEdicoes] = useState<Edicao[]>([]);
 
-        <div className='edicoes-container'>
-            <h1 className='titulo-pagina'>Edições</h1>
+  useEffect(() => {
+    const carregarEdicoes = async () => {
+      try {
+        const response = await axios.get<Edicao[]>('http://localhost:8080/edicoes'); // Substitua '/api/edicoes' pelo endpoint real da sua API
 
-            <div className='edicoes'>
-                <Image
-                    src='/images/covers/JORNAL-01-Novembro-2023.png'
-                    alt='Capa da revista'
-                    width={171}
-                    height={158}
-                    priority={true}
-                />
+        // Ordena as notícias pela data de publicação
+        const edicoesOrdenadas = response.data.sort((a, b) => {
+          const dataA = new Date(a.data).getTime();
+          const dataB = new Date(b.data).getTime();
+          return dataB - dataA; // Ordena do mais recente para o mais antigo
+        });
 
-                <h2>Primeira edição do Jornal Interprofissional</h2>
+        setEdicoes(edicoesOrdenadas);
+      } catch (error) {
+        console.error('Erro ao carregar as edições:', error);
+      }
+    };
 
-                <a className='cor_destaque_link' href='/docs/editions/JORNAL-01-VERSAO-FINA-ESTUDANTE-PARA-ESTUDANTE.pdf' target='_blank'>Download</a>
+    carregarEdicoes();
+  }, []);
 
-            </div>
+  // Função para formatar a data no estilo DD-MM-AAAA
+  const formatarData = (data: string) => {
+    const dataObj = new Date(data);
+    const dia = dataObj.getDate().toString().padStart(2, '0');
+    const mes = (dataObj.getMonth() + 1).toString().padStart(2, '0');
+    const ano = dataObj.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+  };
+
+  return (
+    
+    <div >
+
+      <h1 className="titulo-pagina">Edições</h1>
+
+        <div className="edicoes-container">
+          {edicoes.map((edicao) => (
+            <a key={edicao.id} className="edicao" target="_blank" href={edicao.pdfAddress}>
+              <Image className='imgedicao' src={edicao.imagemAddress} alt="Imagem de capa da edição" width={360} height={360} priority={true} />
+              <p className='tituloedicao'>{edicao.titulo}</p>
+              <span className='dataPublicacao-preview-edicoes'>Publicado em: {formatarData(edicao.data)}</span>
+              <span className='btn-download'>Download</span>
+            </a>
+          ))}
         </div>
-    )
+      </div>
+  );
 }
