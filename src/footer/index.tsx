e6c6647fd1ffe2f6e03df99db7c958da23b99676
Blimabru <bru.no@outlook.com.br>
2024-03-10 21:48:04 -0300

    Alteração no nome do componente

--- src/footer/index.tsx
+++ src/footer/index.tsx
@@ -1,4 +1,4 @@
-import Link_privacy from '../link_privacy'
+import LinkPrivacy from '../linkPrivacy'
 import './style.css'
 
 export default function Footer() {
@@ -14,7 +14,7 @@ export default function Footer() {
                         <li className="lista-rodape"><a href="https://www.instagram.com/claudio.pales" className='link' target='_blank'><span className='cor_destaque_link'>@claudio.pales</span></a>Claudio Pales</li>
                         <li className="lista-rodape"><a href="https://www.instagram.com/jpales_" className='link' target='_blank'><span className='cor_destaque_link'>@jpales_</span></a>João Pedro Pales</li>
                     </ul>
-                    <Link_privacy />
+                    <LinkPrivacy />
                 </div>
             </div>
         </div>
